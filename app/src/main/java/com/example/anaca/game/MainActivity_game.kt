db5de3c58d662bc04de69fb41d50e343a21fc70e
deboracorreia2 <deboracorreia2santos@gmail.com>
2019-04-11 12:16:35 -0300

    Splash Screen e Progress Bar adicionadas ao projeto.

--- app/src/main/java/com/example/anaca/game/MainActivity_game.kt
+++ app/src/main/java/com/example/anaca/game/MainActivity_game.kt
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -17,26 +18,27 @@ class MainActivity_game : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_game)
+        val handle = Handler()
+        handle.postDelayed(Runnable { mostrarMain() }, 3000)
+    }
 
-
-
+    private fun mostrarMain() {
+        val intent = Intent(
+                this@MainActivity_game,
+                NonaPagina::class.java
+        )
+        startActivity(intent)
+        finish()
     }
 
     fun Click(view: View) {
 
-        var intent: Intent = Intent(this,NonaPagina::class.java)
+        var intent: Intent = Intent(this, NonaPagina::class.java)
         startActivity(intent)
-        }
-//meu***************************************************************************************
-
-
-
-
-
-
-
-
-
-
+    }
 }
+
+
+
+
 
