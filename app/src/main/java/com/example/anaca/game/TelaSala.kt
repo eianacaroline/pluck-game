a6eb4423c658f1c406361a2c1ef6be0f925d5f3b
deboracorreia2 <deboracorreia2santos@gmail.com>
2019-04-12 14:46:36 -0300

    Tela sala adicionada

---
+++ app/src/main/java/com/example/anaca/game/TelaSala.kt
@@ -0,0 +1,12 @@
+package com.example.anaca.game
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaSala : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_sala2)
+    }
+}
